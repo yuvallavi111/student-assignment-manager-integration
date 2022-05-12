1edd7f49eaef9f8c2b2b21bde4a59a6fd908f5e5
yuvallavi1 <yuval@byondxr.com>
2022-05-13 00:52:19 +0300

    integration impl

---
+++ src/routes/monday.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+const router = Router();
+
+import * as transformationController from "../controllers/monday-controller";
+import authenticationMiddleware from "../middlewares/authentication";
+
+router.post(
+  "/monday/predict",
+  authenticationMiddleware,
+  transformationController.executeColumnChange
+);
+router.post(
+  "/monday/execute_action",
+  authenticationMiddleware,
+  transformationController.executeAction
+);
+router.post(
+  "/monday/get_remote_list_options",
+  authenticationMiddleware,
+  transformationController.getRemoteListOptions
+);
+
+export default router;
